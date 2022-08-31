fda1cf880234a5e401bb7e98b88939c592371b87
mentionworld <prakashmeghgurung123@gmail.com>
2022-08-31 21:01:38 +0530

    admin Dashboard design

---
+++ orders.js
@@ -0,0 +1,50 @@
+const Order=[
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentSatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'LARVENDER Drone',
+        productNumber: '334131',
+        paymentSatus: 'Refunded',
+        shipping: 'Declined' 
+    },
+    {
+        productName: 'Ruko F11 Drone',
+        productNumber: '85421',
+        paymentSatus: 'Due',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Dronw with Camera Drone',
+        productNumber: '26346',
+        paymentSatus: 'Paid',
+        shipping: 'Delivered' 
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentSatus: 'Done',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentSatus: 'Done',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentSatus: 'Done',
+        shipping: 'Pending' 
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '85631',
+        paymentSatus: 'Done',
+        shipping: 'Pending' 
+    },
+]
